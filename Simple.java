6069dbc081f0e4055209f4c7efaf5e104c84b295
sonaliGitry <sonalivinodpawar@gmail.com>
2020-06-29 10:25:48 +0530

    changes for rid

--- Simple.java
+++ Simple.java
@@ -2,10 +2,7 @@ public class Simple{
 public static void main(String args[]){
 for(int i=0;i<10;i++)
 {
-<<<<<<< HEAD
 System.out.println("hello  ridhansh "+i);
-=======
-System.out.println("hello mayur 11 "+i);
->>>>>>> c682c5ac6aeb44438f20600e0bf62662fc33599d
+
 }
 }}
